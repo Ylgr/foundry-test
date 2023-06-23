352ac91939ca242463e11224bbdd225cc03d6907
viet tu <quyen.luminous@gmail.com>
2023-06-23 19:58:37 +0700

    (+) basic foundry

---
+++ src/TokenERC20.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
+
+
+contract TokenERC20 is Initializable, ERC20BurnableUpgradeable, AccessControlEnumerableUpgradeable {
+    function initialize(string memory name, string memory symbol, uint256 initialSupply) public virtual initializer {
+        __ERC20_init(name, symbol);
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public virtual {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "ERC20Token: must have admin role to mint");
+        _mint(to, amount);
+    }
+}
